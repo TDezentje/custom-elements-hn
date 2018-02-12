7f22dd70e787e79f077a33ccfea9169c6846e53d
Tom Dezentje <tomdezentje@gmail.com>
2018-02-12 20:28:00 +0100

    Removed unused file

--- server/src/settings.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-
-
-const fileContents = fs.readFileSync(path.resolve('settings.json'), 'utf8');
-const Settings = JSON.parse(fileContents);
-
-export {Settings};
